2adea173ee08cbfb9322cfd3c88dac201bcb92f7
Arous Achraf <87370368+Larousse2001@users.noreply.github.com>
2024-06-28 07:36:11 +0100

    Add files via upload

---
+++ donation/src/main/java/tn/esprit/donation/repositories/EmployeRepository.java
@@ -0,0 +1,16 @@
+package tn.esprit.donation.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import tn.esprit.donation.entities.Employe;
+
+import java.util.List;
+
+public interface EmployeRepository extends JpaRepository<Employe, Long> {
+
+    @Query("select e from Employe e join Don d on d.employe =e join CroissantRouge  cr on d member cr.dons where cr.region = :region and e.entreprise.nomEntreprise = :nomentreprise")
+    List<Employe> getEmployeByRegionAndEntreprise(@Param("region") String region, @Param("nomentreprise") String nomentreprise);
+
+
+}
